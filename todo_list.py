d8b61d9de8e9dddd18d6ea3d73009e27d2494eb3
Geethanjali-pandiri <pandirigeethanjali3@gmail.com>
2026-06-11 17:06:18 +0530

    Add files via upload

---
+++ todo_list.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+root = tk.Tk()
+root.title("To-Do List")
+root.geometry("400x400")
+def add_task():
+    task = task_entry.get()
+
+    if task != "":
+        task_listbox.insert(tk.END, task)
+        task_entry.delete(0, tk.END)
+def delete_task():
+    selected_task = task_listbox.curselection()
+
+    if selected_task:
+        task_listbox.delete(selected_task)
+def save_tasks():
+    with open("tasks.txt", "w") as file:
+        tasks = task_listbox.get(0, tk.END)
+
+        for task in tasks:
+            file.write(task + "\n")
+def load_tasks():
+    try:
+        with open("tasks.txt", "r") as file:
+            for task in file:
+                task_listbox.insert(tk.END, task.strip())
+    except FileNotFoundError:
+        pass
+task_entry = tk.Entry(root, width=30)
+task_entry.pack(pady=10)
+add_button = tk.Button(root, text="Add Task",command=add_task)
+add_button.pack(pady=5)
+task_listbox = tk.Listbox(root, width=40, height=10)
+task_listbox.pack(pady=10)
+delete_button = tk.Button(root, text="Delete Task", command=delete_task)
+delete_button.pack(pady=5)
+save_button = tk.Button(root, text="Save Tasks", command=save_tasks)
+save_button.pack(pady=5)
+load_tasks()
+root.mainloop()
